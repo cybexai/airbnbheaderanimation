234e98ddf73a4e16da09cec61c64945d3e1dc02f
cybex237 <cybexsa@gmail.com>
2020-01-17 03:06:20 +0100

    Animated and styled the header

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Platform, StatusBar } from 'react-native';
+import Animated from 'react-native-reanimated';
 
 const images = [
   { id: 1, uri: require('./assets/images/1.jpg')},
@@ -9,15 +10,41 @@ const images = [
   { id: 5, uri: require('./assets/images/5.png')},
 ];
 
+const scrollY = new Animated.Value(0);
+
+const HEADER_HEIGHT = Platform.OS == 'ios'?115:70
++StatusBar.currentHeight;
+
+const diffClampScrollY = Animated.diffClamp(scrollY,0,HEADER_HEIGHT)
+
+const headerY = Animated.interpolate(diffClampScrollY,{
+  inputRange: [0, HEADER_HEIGHT],
+  outputRange: [0, -HEADER_HEIGHT]
+})
+
+
 export default class App extends React.Component {
-  
+    
   render() {
     return(
       <View style={{flex:1}}>
 
-      <View style={styles.header} />
+      <Animated.View style={styles.header}> 
 
-      <ScrollView>
+      <Text>Animated Header</Text>
+
+      </Animated.View>
+
+      <Animated.ScrollView
+      contentContainerStyle={styles.scrollView}
+      bounces={false}
+      scrollEventThrottle={16}
+      onScroll={Animated.event([
+        {
+          nativeEvent: { contentOffset:{y : scrollY} }
+        }
+      ])}
+      >
       {images.map(image => (
         <View key={image.id} style={{height:400, margin:20}}>
 
@@ -28,34 +55,34 @@ export default class App extends React.Component {
 
         </View>
       ))}
-      </ScrollView>
-        
+      </Animated.ScrollView>
+              
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
+const styles = StyleSheet.create({  
   header: {
     position: 'absolute',
     left: 0,
     right: 0,
     top: 0,
-    height: 70,
+    height: HEADER_HEIGHT,
     backgroundColor: 'grey',
     zIndex: 1000,
-    elevation: 1000
+    elevation: 1000,
+    transform: [{ translateY: headerY }],
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   image: {
     flex: 1,
     height: null,
     width: null,
     borderRadius: 10
+  },
+  scrollView: {
+    paddingTop: HEADER_HEIGHT
   }
 })
